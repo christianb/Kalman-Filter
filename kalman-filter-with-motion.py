8e1f42691197a89e87dd1c33246b126a3bfcf941
Christian Bunk <376601+christianb@users.noreply.github.com>
2019-07-19 01:14:21 +0200

    use extracted class

--- kalman-filter-with-motion.py
+++ kalman-filter-with-motion.py
@@ -1,54 +1,7 @@
 import random
 import numpy as np
 import matplotlib.pyplot as plt
-
-class KalmanFilter(object):
-
-	def __init__(self, r, q, b = 0): 
-		self.R = r 
-		self.Q = q
-		self.x = 0.0
-		self.A = 1
-		self.B = b # Control vector
-		self.C = 1
-		self.cov = 0.0
-
-	# predicts the next value
-	# u - control
-	def predict(self, u = 0):
-		return (self.A * float(self.x)) + (self.B * u)
-
-	def uncertainty(self):
-		return ((self.A * float(self.cov)) * self.A) + self.R
-
-	# filter
-	def filter(self, measurement, u=0):
-		if (self.x == 0.0):
-			# print("x == 0.0")
-			self.x = (1 / self.C) * measurement
-			# print("x = ", self.x)
-			self.cov = (1 / self.C) * self.Q * (1 / self.C)
-			# print("cov = ", self.cov)
-		else:
-			predX = self.predict(u)
-			# print("predX = ", predX)
-
-			predCov = self.uncertainty()
-			# print("predCov = ", predCov, (" uncertainty"))
-
-			# Kalman Gain
-			k_gain = predCov * self.C * (1 / ((self.C * predCov * self.C) + self.Q))
-			# k_gain = 2.0 / 3.0
-			# print("k_gain = ", k_gain)
-
-			# Correction
-			self.x = predX + k_gain * (measurement - (self.C * predX))
-			# print("corrected x = ", self.x)
-			self.cov = predCov - (k_gain * self.C * predCov)
-			# print("corrected cov = ", self.cov)
-
-		# print ("filter result = ", self.x)
-		return self.x
+from kalman_filter import KalmanFilter
 
 # returns a gausian measurments around rssi value
 def measurments(rssi, x_axis):
@@ -96,8 +49,8 @@ def main():
 	
 	B = 0.5
 
-	kalman_with_motion = KalmanFilter(R, Q, B)
-	kalman_plain = KalmanFilter(R,Q,0)
+	kalman_with_motion = KalmanFilter(R, Q, b=B)
+	kalman_plain = KalmanFilter(R,Q,b=0)
 
 	# define a range of x values
 	x_axis = np.arange(0, 40, 0.5)
