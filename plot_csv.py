af2c5dfe5a59921cccf13935415213eafcb1146f
Christian Bunk <376601+christianb@users.noreply.github.com>
2019-07-19 19:46:42 +0200

    save plots

--- plot_csv.py
+++ plot_csv.py
@@ -1,25 +1,29 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# returns a gausian measurments around rssi value
+def plot_csv(filename):	
+	data = np.loadtxt("csv/"+filename+".csv", unpack = False, delimiter=',')
 
-def plot():
-
-	data = np.loadtxt('rssi.csv', unpack = False, delimiter=',')
-	print(data)
+	rssi = data[0]
+	average = data[1]
+	kalman = data[2]
 	
-#plt.figure()
-	#plt.plot(x_axis, rssi, 'bo', label='RSSI')
-	#plt.plot(x_axis, average, 'g', label='average')
-	#plt.plot(x_axis, kalman, 'r', label='kalman')
-	#plt.legend()
-	#plt.show()	
+	x_axis = np.arange(0, len(rssi), 1)
 
+	plt.figure()
+	plt.ylabel("RSSI in dB")
+	plt.plot(x_axis, rssi, 'bo', label='raw')
+	plt.plot(x_axis, average, 'g', label='average')
+	plt.plot(x_axis, kalman, 'r', label='kalman')
+	plt.legend()
+	out_name = filename+".png"
+	plt.savefig("plots/"+out_name)
+	# plt.show()
 
 def main():
-	plot()
+	plot_csv('rssi_1')
+	plot_csv('rssi_2')
 	
-
 
 if __name__ == '__main__':
 	main()
