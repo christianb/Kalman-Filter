4f7441c3c6261140dcf0c75efd8b6c88dfb4d9fe
Christian Bunk <376601+christianb@users.noreply.github.com>
2019-07-20 00:36:08 +0200

    add png

---
+++ main.py
@@ -0,0 +1,34 @@
+from plot import plot
+from kalman_filter import KalmanFilter
+import numpy as np
+
+def main():
+	rssi = np.loadtxt("rssi_2.csv", unpack = False, delimiter=',')
+
+	# print(rssi)
+	
+	# R and Q affect the peaks of the kalman graph. 
+  # Its the relationship between R and Q
+	# The task is to find good R,Q values
+
+	R = 0.01 # system noise
+	Q = 5 # measurement noise
+
+	A = 1
+	B = 0
+	C = 1
+
+	kalman = KalmanFilter(R, Q, A, B, C)
+	
+	g = []
+	for x in range(len(rssi)):
+		g.append(kalman.filter(rssi[x]))
+   
+	k_filtered = np.asarray(g)
+
+	title = "R: " + str(R) + ", Q: " + str(Q) + ", A: " + str(A) + ", B: " + str(B) + ", C: " + str(C)
+	plot(rssi, k_filtered, "test_17.png", title)
+
+
+if __name__ == '__main__':
+	main()
---
+++ plot.py
@@ -0,0 +1,32 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def plot_csv(filename):	
+	data = np.loadtxt("csv/"+filename+".csv", unpack = False, delimiter=',')
+
+	rssi = data[0]
+	average = data[1]
+	kalman = data[2]
+	
+	x_axis = np.arange(0, len(rssi), 1)
+
+	plt.figure()
+	plt.ylabel("RSSI in dB")
+	plt.plot(x_axis, rssi, 'bo', label='raw')
+	plt.plot(x_axis, average, 'g', label='average')
+	plt.plot(x_axis, kalman, 'r', label='kalman')
+	plt.legend()
+	out_name = filename+".png"
+	plt.savefig("plots/"+out_name)
+	# plt.show()
+
+def plot(raw, kalman, filename, title):
+	x_axis = np.arange(0, len(raw), 1)
+	
+	plt.figure()
+	plt.title(title)
+	plt.ylabel("RSSI in dB")
+	plt.plot(x_axis, raw, 'bo', label='raw')
+	plt.plot(x_axis, kalman, 'r', label='kalman')
+	plt.legend()
+	plt.savefig(filename)
