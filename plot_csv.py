462949f4344f4595ea71da1768b6aafd9a0aa62c
Christian Bunk <376601+christianb@users.noreply.github.com>
2019-07-20 17:15:41 +0200

    update file

--- plot_csv.py
+++ plot_csv.py
@@ -22,12 +22,12 @@ def plot_csv(filename):
 
 def main():
 	plot_csv('rssi_10:A2:EF:30:0C:03')
-	plot_csv('rssi_30:1B:C8:DE:D1:75')
-	plot_csv('rssi_5A:49:CD:F7:F0:CA')
+	plot_csv('rssi_3E:A5:87:67:B5:22')
+	plot_csv('rssi_50:32:37:BA:ED:39')
 	plot_csv('rssi_76:FD:71:07:9B:42')
 	plot_csv('rssi_84:C0:EF:E0:55:B9')
-	plot_csv('rssi_88:C6:26:AE:08:C7')
-	plot_csv('rssi_88:C6:26:AE:08:C7')
+	plot_csv('rssi_7B:A3:9E:88:9E:1D')
+	plot_csv('rssi_D8:E0:E1:0E:97:81')
 	
 
 if __name__ == '__main__':
