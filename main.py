4fa854b09c5bc666958273edc804b8ca36ecf283
Christian Bunk <376601+christianb@users.noreply.github.com>
2019-07-20 16:19:11 +0200

    add plots

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ def main():
   # Its the relationship between R and Q
 	# The task is to find good R,Q values
 
-	R = 0.01 # system noise
-	Q = 5 # measurement noise
+	R = 0.05 # system noise
+	Q = 3 # measurement noise
 
 	A = 1
 	B = 0
@@ -27,7 +27,7 @@ def main():
 	k_filtered = np.asarray(g)
 
 	title = "R: " + str(R) + ", Q: " + str(Q) + ", A: " + str(A) + ", B: " + str(B) + ", C: " + str(C)
-	plot(rssi, k_filtered, "test_17.png", title)
+	plot(rssi, k_filtered, "test_24.png", title)
 
 
 if __name__ == '__main__':
